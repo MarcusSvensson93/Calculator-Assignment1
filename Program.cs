ae380f1569f1800cd840f080dcc1249353b006dd
MarcusSvensson93 <89707113+MarcusSvensson93@users.noreply.github.com>
2021-09-07 20:43:38 +0200

    Add files via upload
    Cleaned up the code by moving my method around so it's not withing another method.

---
+++ Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace Calculator_Assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to Marcus Svenssons Calculator. Please select Calculator");
+
+            bool keepLooping = true;
+
+            while (keepLooping)
+            {
+                Console.WriteLine("---- Menu ----\n1. Calculator\n2. Exit");
+
+                int selection = AskUserForNumber("Your Selection");
+
+                switch (selection)
+                {
+                    case 1:
+                        Calculator();
+
+                        break;
+
+                    case 2:
+                        keepLooping = false;
+                        Console.WriteLine("Shuting down program");
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        }
+        static void Calculator()
+        {
+            Console.WriteLine("You may now use the Calculator!");
+            Console.WriteLine("OPS: While using Division, you can't divide by 0");
+
+            Console.Write("Enter a number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter Operator: ");
+            string op = Console.ReadLine();
+
+            Console.Write("Enter a number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            if (op == "+")
+            {
+                Console.WriteLine(num1 + num2);
+            }
+            else if (op == "-")
+            {
+                Console.WriteLine(num1 - num2);
+            }
+            else if (op == "/")
+            {
+                Console.WriteLine(num1 / num2);
+            }
+            else if (op == "*")
+            {
+                Console.WriteLine(num1 * num2);
+            }
+
+            else
+            {
+                Console.WriteLine("Invalid Operator");
+            }
+        }
+
+        static int AskUserForNumber(string what)
+        {
+            bool wasNotNumber = true;
+            int number = 0;
+            do
+            {
+                wasNotNumber = !int.TryParse(AskUserFor(what), out number);
+
+            } while (wasNotNumber);
+
+            return number;
+
+        }
+
+        static string AskUserFor(string what)
+        {
+            Console.Write("Please enter " + what + ": ");
+
+            string userInput = Console.ReadLine();
+
+            return userInput;
+        }
+    }
+}
